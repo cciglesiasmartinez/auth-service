0484f9e29403acb0d08877ab878ed61501360335
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-03 06:32:13 +0200

    Adds main class and test class for auth-service.

---
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.filmdb.auth.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.filmdb.auth.auth_service.repository")
+@EntityScan(basePackages = "com.filmdb.auth.auth_service.entity")
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
