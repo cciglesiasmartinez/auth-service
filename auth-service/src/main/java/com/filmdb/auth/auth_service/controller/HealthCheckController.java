41c3fbf821ce4f2efabc24a9c1c2a4eeb7c19cfa
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-03 06:30:09 +0200

    Adds HealthCheckController class
    Debugging and testing purposes.

---
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.filmdb.auth.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Test class.
+ */
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/ping")
+    public String ping() {
+        System.out.println("==> /ping endpoint was called :3");
+        return "Auth API is active :3";
+    }
+}
