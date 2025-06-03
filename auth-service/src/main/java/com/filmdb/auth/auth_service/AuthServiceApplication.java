b5ec189197b70488698df1578e0408d6bf97fb38
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-03 10:15:11 +0200

    Adds Swagger/OpenAPI dependency and basic configuration.

--- auth-service/src/main/java/com/filmdb/auth/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/AuthServiceApplication.java
@@ -1,10 +1,13 @@
 package com.filmdb.auth.auth_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(info = @Info(title = "Auth Service API", version = "v1"))
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.filmdb.auth.auth_service.repository")
 @EntityScan(basePackages = "com.filmdb.auth.auth_service.entity")
--- auth-service/src/main/java/com/filmdb/auth/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/config/SecurityConfig.java
@@ -10,8 +10,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()            // Deshabilita CSRF (necesario para pruebas POST sin token)
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
         return http.build();
     }
 }
