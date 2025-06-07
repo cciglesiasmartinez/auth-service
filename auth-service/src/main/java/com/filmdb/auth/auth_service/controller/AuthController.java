8497cc68485d4e35686ac80c491ed2e725a2044e
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-07 06:06:29 +0200

    Adds /me/password route to AuthController

--- auth-service/src/main/java/com/filmdb/auth/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.filmdb.auth.auth_service.controller;
 
+import com.filmdb.auth.auth_service.dto.ChangePasswordRequest;
 import com.filmdb.auth.auth_service.dto.LoginRequest;
 import com.filmdb.auth.auth_service.dto.LoginResponse;
 import com.filmdb.auth.auth_service.dto.RegisterRequest;
@@ -7,7 +8,6 @@ import com.filmdb.auth.auth_service.entity.User;
 import com.filmdb.auth.auth_service.security.CustomUserDetails;
 import com.filmdb.auth.auth_service.service.AuthService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -24,7 +24,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest request) {
         try {
             User newUser = authService.registerUser(
                     request.getUsername(),
@@ -39,9 +39,9 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest request) {
         try {
-            LoginResponse loginResponse = authService.login(
+            LoginResponse loginResponse = authService.loginUser(
                     request.getEmail(),
                     request.getPassword()
             );
@@ -61,4 +61,21 @@ public class AuthController {
         User user = userDetails.getUser();
         return ResponseEntity.ok(user);
     }
+
+    @PutMapping("/me/password")
+    public ResponseEntity<?> changeUserPassword(@RequestBody ChangePasswordRequest request,
+                                                Authentication authentication) {
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        User user = userDetails.getUser();
+        try {
+            authService.changeUserPassword(user, request.getCurrentPassword(), request.getNewPassword());
+            return ResponseEntity.ok("Password changed.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
 }
