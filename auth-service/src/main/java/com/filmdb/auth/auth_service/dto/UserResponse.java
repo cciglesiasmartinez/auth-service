355695aa9e0553342e89266d8c5561ca8e38e05a
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-09 04:18:47 +0200

    Adds UserResponse DTO
    Intended to be a mask for the User (entity User) object. In production should not show password, userId and other sensible information. However, for development purposes I'm choosing to disclose every single attribute.

---
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/dto/UserResponse.java
@@ -0,0 +1,35 @@
+package com.filmdb.auth.auth_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.filmdb.auth.auth_service.entity.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserResponse {
+
+    private String id;
+    private String username;
+    private String email;
+    private String password; // Remove before production
+    private boolean isAdmin;
+    private LocalDateTime registeredAt;
+    private LocalDateTime modifiedAt;
+
+    public static UserResponse fromUser(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword()) // Remove before production deployment
+                .isAdmin(user.isAdmin())
+                .registeredAt(user.getRegisteredAt())
+                .modifiedAt(user.getModifiedAt())
+                .build();
+    }
+}
