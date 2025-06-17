74d1f7d29f40d533e497ed1a946ba070b23153e1
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-17 13:00:23 +0200

    Disables MapStruct for AuthRequestCommandMapper

--- auth-service/src/main/java/com/filmdb/auth/auth_service/adapter/in/web/mapper/AuthRequestCommandMapper.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/adapter/in/web/mapper/AuthRequestCommandMapper.java
@@ -1,31 +1,38 @@
 package com.filmdb.auth.auth_service.adapter.in.web.mapper;
 
+import com.filmdb.auth.auth_service.adapter.in.web.dto.requests.*;
 import com.filmdb.auth.auth_service.application.commands.*;
-import com.filmdb.auth.auth_service.dto.requests.*;
-import org.mapstruct.Context;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+//@Mapper(componentModel = "spring")
 public interface AuthRequestCommandMapper {
 
     RegisterUserCommand toRegisterUserCommand(RegisterRequest request);
 
     LoginUserCommand toLoginUserCommand(LoginRequest request);
 
-    default ChangePasswordCommand toChangePasswordCommand(ChangePasswordRequest request, @Context String userId) {
-        return new ChangePasswordCommand(userId, request.getCurrentPassword(), request.getNewPassword());
-    }
+//    default ChangePasswordCommand toChangePasswordCommand(ChangePasswordRequest request, @Context String userId) {
+//        return new ChangePasswordCommand(userId, request.getCurrentPassword(), request.getNewPassword());
+//    }
+//
+//    default ChangeUserEmailCommand toChangeUserEmailCommand(ChangeEmailRequest request, @Context String userId) {
+//        return new ChangeUserEmailCommand(userId, request.getCurrentPassword(), request.getEmail());
+//    }
+//
+//    default ChangeUserUsernameCommand toChangeUserUsernameCommand(ChangeUsernameRequest request, @Context String userId) {
+//        return new ChangeUserUsernameCommand(userId, request.getCurrentPassword(), request.getUsername());
+//    }
+//
+//    default DeleteUserCommand toDeleteUserCommand(DeleteUserRequest request, @Context String userId) {
+//        return new DeleteUserCommand(userId, request.getCurrentPassword());
+//    }
 
-    default ChangeUserEmailCommand toChangeUserEmailCommand(ChangeEmailRequest request, @Context String userId) {
-        return new ChangeUserEmailCommand(userId, request.getCurrentPassword(), request.getEmail());
-    }
+    ChangePasswordCommand toChangePasswordCommand(ChangePasswordRequest request, String userId);
 
-    default ChangeUserUsernameCommand toChangeUserUsernameCommand(ChangeUsernameRequest request, @Context String userId) {
-        return new ChangeUserUsernameCommand(userId, request.getCurrentPassword(), request.getUsername());
-    }
+    ChangeUserEmailCommand toChangeUserEmailCommand(ChangeEmailRequest request, String userId);
 
-    default DeleteUserCommand toDeleteUserCommand(DeleteUserRequest request, @Context String userId) {
-        return new DeleteUserCommand(userId, request.getCurrentPassword());
-    }
+    ChangeUserUsernameCommand toChangeUserUsernameCommand(ChangeUsernameRequest request, String userId);
+
+    DeleteUserCommand toDeleteUserCommand(DeleteUserRequest request, String userId);
+
 
 }
