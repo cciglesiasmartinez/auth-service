d7513c5e5b1316ee520f6dd215d8d8f23978539a
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-17 09:02:47 +0200

    Annotates SpringDataUserRepository as Repository

--- auth-service/src/main/java/com/filmdb/auth/auth_service/infrastructure/persistence/repository/SpringDataUserRepository.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/infrastructure/persistence/repository/SpringDataUserRepository.java
@@ -1,18 +1,17 @@
 package com.filmdb.auth.auth_service.infrastructure.persistence.repository;
 
-import com.filmdb.auth.auth_service.domain.model.Email;
-import com.filmdb.auth.auth_service.domain.model.UserId;
-import com.filmdb.auth.auth_service.domain.model.Username;
 import com.filmdb.auth.auth_service.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SpringDataUserRepository extends JpaRepository<UserEntity, String> {
 
-    Optional<UserEntity> findById(UserId id);
-    Optional<UserEntity> findByEmail(Email email);
-    Optional<UserEntity> findByUsername(Username username);
+    Optional<UserEntity> findById(String id);
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
 //    Optional<UserEntity> save(UserEntity user);
     void delete(UserEntity user);
     boolean existsByEmailOrUsername(String email, String username);
