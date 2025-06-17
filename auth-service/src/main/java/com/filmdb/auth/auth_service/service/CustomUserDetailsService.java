be670d0404a532ad1f8ee9faa57c0d782fdf545f
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-06-17 09:02:20 +0200

    Fixes CustomUserDetailsServices class to work with new User class from domain

--- auth-service/src/main/java/com/filmdb/auth/auth_service/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/service/CustomUserDetailsService.java
@@ -1,7 +1,8 @@
 package com.filmdb.auth.auth_service.service;
 
-import com.filmdb.auth.auth_service.entity.User;
-import com.filmdb.auth.auth_service.repository.UserRepository;
+import com.filmdb.auth.auth_service.domain.model.User;
+import com.filmdb.auth.auth_service.domain.model.Username;
+import com.filmdb.auth.auth_service.domain.repository.UserRepository;
 import com.filmdb.auth.auth_service.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
@@ -17,11 +18,14 @@ public class CustomUserDetailsService  implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> optionalUser = userRepository.findByUsername(username);
-        User user = optionalUser.get();
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found with username: " + username);
-        }
-        return new CustomUserDetails(user);
+//        Optional<User> optionalUser = userRepository.findByUsername(Username.of(username));
+//        User user = optionalUser.get();
+//        if (user == null) {
+//            throw new UsernameNotFoundException("User not found with username: " + username);
+//        }
+//        return new CustomUserDetails(user);
+        return userRepository.findByUsername(Username.of(username))
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 }
