c39f71f0dd77b4c31f51c18cef1a93227c5f518c
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-07-09 09:12:58 +0200

    Updates RedisConfig for verificationCodeRepository

--- auth-service/src/main/java/com/filmdb/auth/auth_service/infrastructure/persistence/redis/config/RedisConfig.java
+++ auth-service/src/main/java/com/filmdb/auth/auth_service/infrastructure/persistence/redis/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.filmdb.auth.auth_service.domain.model.RefreshToken;
+import com.filmdb.auth.auth_service.domain.model.VerificationCode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,8 +15,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisTemplate<String, RefreshToken> redisTemplate(RedisConnectionFactory connectionFactory) {
+    @Bean(name = "refreshTokenRedisTemplate")
+    public RedisTemplate<String, RefreshToken> refreshTokenRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, RefreshToken> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -27,6 +28,28 @@ public class RedisConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.findAndRegisterModules();
+        valueSerializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(valueSerializer);
+        template.setHashValueSerializer(valueSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean(name = "VerificationCodeRedisTemplate")
+    public RedisTemplate<String, VerificationCode> verificationCodeRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, VerificationCode> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+
+        Jackson2JsonRedisSerializer<VerificationCode> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(VerificationCode.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable((SerializationFeature.WRITE_DATES_AS_TIMESTAMPS));
         mapper.findAndRegisterModules();
         valueSerializer.setObjectMapper(mapper);
 
