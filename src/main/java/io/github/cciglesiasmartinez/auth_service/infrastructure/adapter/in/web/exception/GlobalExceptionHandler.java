92e501d99d8419e8c0b16aea0631f67d4a62224f
cassiezer0 <cciglesiasmartinez@gmail.com>
2025-09-06 02:50:58 +0200

    Improves log msg format.
    Adds exceptionOrigin field to Slf4j logging, providing better traceability for thrown exceptions. Also centralizes all exception logging in GlobalExceptionHandler class.

--- src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/adapter/in/web/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/adapter/in/web/exception/GlobalExceptionHandler.java
@@ -5,21 +5,35 @@ import io.github.cciglesiasmartinez.auth_service.domain.exception.*;
 import io.github.cciglesiasmartinez.auth_service.infrastructure.adapter.in.web.dto.responses.Envelope;
 import io.github.cciglesiasmartinez.auth_service.infrastructure.adapter.in.web.dto.responses.ExceptionResponse;
 import io.github.cciglesiasmartinez.auth_service.infrastructure.adapter.in.web.dto.responses.Meta;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private ResponseEntity<Envelope<ExceptionResponse>> buildResponse(String code, HttpStatus type) {
+    private ResponseEntity<Envelope<ExceptionResponse>> buildResponse(Exception e, String code, HttpStatus type) {
+        StackTraceElement origin = e.getStackTrace()[0];
+        MDC.put("exceptionOrigin",
+                origin.getClassName() + ":" + origin.getLineNumber());
+        log.warn(e.getMessage());
+        MDC.remove("exceptionOrigin");
         ExceptionResponse exceptionResponse = new ExceptionResponse(code);
         Envelope<ExceptionResponse> response = new Envelope<>(exceptionResponse, new Meta());
         return new ResponseEntity<>(response, type);
     }
 
-    private ResponseEntity<Envelope<ExceptionResponse>> buildResponse(String code, String message, HttpStatus type) {
+    private ResponseEntity<Envelope<ExceptionResponse>> buildResponse(Exception e, String code, String message,
+                                                                      HttpStatus type) {
+        StackTraceElement origin = e.getStackTrace()[0];
+        MDC.put("exceptionOrigin",
+                origin.getClassName() + ":" + origin.getLineNumber());
+        log.warn(e.getMessage());
+        MDC.remove("exceptionOrigin");
         ExceptionResponse exceptionResponse = new ExceptionResponse(code, message);
         Envelope<ExceptionResponse> response = new Envelope<>(exceptionResponse, new Meta());
         return new ResponseEntity<>(response, type);
@@ -27,67 +41,67 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserAlreadyRegisteredException.class)
     public ResponseEntity<?> handleUserAlreadyRegistered(UserAlreadyRegisteredException e) {
-        return buildResponse("user_already_registered", HttpStatus.CONFLICT);
+        return buildResponse(e, "user_already_registered", HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<?> handleInvalidCredentialsException(InvalidCredentialsException e) {
-        return buildResponse("invalid_credentials", HttpStatus.UNAUTHORIZED);
+        return buildResponse(e, "invalid_credentials", HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     public ResponseEntity<?> handlePasswordMismatchException(PasswordMismatchException e) {
-        return buildResponse("password_mismatch", HttpStatus.BAD_REQUEST);
+        return buildResponse(e, "password_mismatch", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<?> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
-        return buildResponse("email_already_exists", HttpStatus.CONFLICT);
+        return buildResponse(e, "email_already_exists", HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<?> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e) {
-        return buildResponse("username_already_exists", HttpStatus.CONFLICT);
+        return buildResponse(e, "username_already_exists", HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
     public ResponseEntity<?> handleInvalidPasswordException(InvalidPasswordException e ) {
-        return buildResponse("invalid_password", e.getMessage(), HttpStatus.BAD_REQUEST);
+        return buildResponse(e, "invalid_password", e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidUsernameException.class)
     public ResponseEntity<?> handleInvalidUsernameException(InvalidUsernameException e ) {
-        return buildResponse("invalid_username", HttpStatus.BAD_REQUEST);
+        return buildResponse(e, "invalid_username", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException e) {
-        return buildResponse("user_not_found", HttpStatus.UNAUTHORIZED);
+        return buildResponse(e, "user_not_found", HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UserIsExternalException.class)
     public ResponseEntity<?> handleUserIsExternalException(UserIsExternalException e) {
-        return buildResponse("unauthorized", HttpStatus.UNAUTHORIZED);
+        return buildResponse(e, "unauthorized", HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UserIsNotExternalException.class)
     public ResponseEntity<?> handleUserIsNotExternalException(UserIsNotExternalException e) {
-        return buildResponse("user_is_not_external", HttpStatus.UNAUTHORIZED);
+        return buildResponse(e, "user_is_not_external", HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(VerificationCodeNotFoundException.class)
     public ResponseEntity<?> handleVerificationCodeNotFoundException(VerificationCodeNotFoundException e) {
-        return buildResponse("verification_code_not_found", HttpStatus.UNAUTHORIZED);
+        return buildResponse(e, "verification_code_not_found", HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(RefreshTokenExpiredException.class)
     public ResponseEntity<?> handleRefreshTokenExpiredException(RefreshTokenExpiredException e ) {
-        return buildResponse("refresh_token_expired", HttpStatus.BAD_REQUEST);
+        return buildResponse(e, "refresh_token_expired", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RefreshTokenNotFoundException.class)
     public ResponseEntity<?> handleRefreshTokenNotFoundException(RefreshTokenNotFoundException e) {
-        return buildResponse("refresh_token_not_found", HttpStatus.BAD_REQUEST);
+        return buildResponse(e, "refresh_token_not_found", HttpStatus.BAD_REQUEST);
     }
 
 }
