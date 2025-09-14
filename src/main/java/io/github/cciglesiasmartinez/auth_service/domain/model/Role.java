41b5f5e002556383c8203c7847158cc83a5624ac
cassiezer0 <cciglesiasmartinez@gmail.com>
2025-09-14 20:18:45 +0200

    Adds private constructor, factory method and getters for Role domain entity.

--- src/main/java/io/github/cciglesiasmartinez/auth_service/domain/model/Role.java
+++ src/main/java/io/github/cciglesiasmartinez/auth_service/domain/model/Role.java
@@ -6,4 +6,26 @@ public class Role {
     private String name;
     private String description;
 
+    private Role(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public static Role create(String id, String name, String description) {
+        return new Role(id, name, description);
+    }
+
+    public String id() {
+        return this.id;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public String description() {
+        return this.description;
+    }
+
 }
