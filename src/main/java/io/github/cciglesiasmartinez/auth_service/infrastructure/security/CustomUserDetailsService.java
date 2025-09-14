e45485fbfd39452bf9b522085da07babb0ebce28
cassiezer0 <cciglesiasmartinez@gmail.com>
2025-09-15 00:24:44 +0200

    Adds role authorities in CustomUserDetailsService.

--- src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/security/CustomUserDetailsService.java
@@ -1,12 +1,18 @@
 package io.github.cciglesiasmartinez.auth_service.infrastructure.security;
 
 import io.github.cciglesiasmartinez.auth_service.domain.exception.UserNotFoundException;
+import io.github.cciglesiasmartinez.auth_service.domain.model.User;
 import io.github.cciglesiasmartinez.auth_service.domain.model.valueobject.UserId;
 import io.github.cciglesiasmartinez.auth_service.domain.model.valueobject.Username;
 import io.github.cciglesiasmartinez.auth_service.domain.port.out.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class CustomUserDetailsService  implements UserDetailsService {
@@ -22,6 +28,13 @@ public class CustomUserDetailsService  implements UserDetailsService {
     }
 
     public UserDetails loadUserByUserId(String userId) throws UserNotFoundException {
+        User user = userRepository.findById(UserId.of(userId)).orElseThrow(UserNotFoundException::new);
+        System.out.println(user.toString());
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        user.roles().forEach(
+                role -> {
+                    authorities.add(new SimpleGrantedAuthority(role.name()));
+                });
         return userRepository.findById(UserId.of(userId))
                 .map(CustomUserDetails::new)
                 .orElseThrow(() -> new UserNotFoundException());
