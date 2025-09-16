47100608a584b4e9e66ec274961e155f95e25647
cassiezer0 <cciglesiasmartinez@gmail.com>
2025-09-16 23:28:51 +0200

    Implements new getAuthorities() method.

--- src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/security/CustomUserDetails.java
+++ src/main/java/io/github/cciglesiasmartinez/auth_service/infrastructure/security/CustomUserDetails.java
@@ -2,10 +2,11 @@ package io.github.cciglesiasmartinez.auth_service.infrastructure.security;
 
 import io.github.cciglesiasmartinez.auth_service.domain.model.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
 
@@ -15,14 +16,22 @@ public class CustomUserDetails implements UserDetails {
         this.user = user;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.roles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toSet());
+    }
+
+
     public User getUser()  {
         return user;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of();
+//    }
 
     @Override
     public String getPassword() {
